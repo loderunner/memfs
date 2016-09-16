59ece2cdf0d0043ee9ebf1c5214e04ce2464db82
Charles Francoise <charles.francoise@gmail.com>
2016-09-16 12:19:13 +0200

    Initial Commit

---
+++ memfs/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  memfs
+//
+//  Created by Charles Francoise on 16/09/16.
+//  Copyright © 2016 Charles Francoise. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
